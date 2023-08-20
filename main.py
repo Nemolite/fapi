4ae4b1629732e9782a417f942f614e97d992f1cb
Nemolite <g25092011@mail.ru>
2023-08-20 16:55:57 +0300

    get

--- main.py
+++ main.py
@@ -16,3 +16,15 @@ async def root():
 @app.get("/get_data/{id}")
 async def root(id: int):
     return [d for d in fake_data if d.get("id")==id ]
+
+fake_trades = [
+    {"id":1,"user_id":1, "currency":"BTC","price": 1000},
+    {"id":2,"user_id":1, "currency":"BTC","price": 2000},
+    {"id":3,"user_id":1, "currency":"BTC","price": 5000},
+    {"id":4,"user_id":1, "currency":"BTC","price": 7000},
+]
+
+@app.get("/traders")
+def get_trades(limit:int, offset:int):
+    return fake_trades[offset:][:limit]
+
