d744eaaec277e6a76498fa5d72332615e05c4613
Nemolite <g25092011@mail.ru>
2023-09-12 20:07:54 +0300

    аÐвалидация

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
 app = FastAPI()
 
@@ -38,3 +39,23 @@ class User(BaseModel):
 @app.get("/users/{user_id}",response_model=list[User])
 def get_user(user_id:int):
     return [ user for user in data if user.get("id")==user_id]
+
+class TypeRols(Enum):
+    junior = "junior"
+    middle = "middle"
+    sinuor = "sinuor"
+
+class Rols():
+    id:int
+    name_rols:TypeRols
+
+
+myusers = [
+    {"id":1,"name":"Билл","age":52,"step":200.00,"role":[{"id":1,"name_rols":"junior"}]},
+    {"id":2,"name":"Илон","age":42,"step":300.00,"role":[{"id":1,"name_rols":"sinuor"}]}
+]
+
+
+@app.get("/us/{user_id}")
+def get_user(user_id:int):
+    return [ user for user in myusers if user.get("id")==user_id]
