4102a39ae7c32b6b3543f9bc32f8343ec58590f4
Nemolite <g25092011@mail.ru>
2023-09-12 07:30:54 +0300

    query

--- main.py
+++ main.py
@@ -51,4 +51,14 @@ async def get_model(model_name: ModelName):
 
     return {"model_name": model_name, "message": "Have some residuals"}
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# http://127.0.0.1:8000/items/?skip=0&limit=10
+
+print(fake_items_db[1:2])
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
 
