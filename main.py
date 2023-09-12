a87a5333f5a1a4d39048106316c7077935091c97
Nemolite <g25092011@mail.ru>
2023-09-12 07:49:34 +0300

    bool

--- main.py
+++ main.py
@@ -59,6 +59,21 @@ print(fake_items_db[1:2])
 
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
+    # Обратите внимание тут срез
     return fake_items_db[skip : skip + limit]
 
+@app.get("/itemsbool/{item_id}")
+# short - объявлен как логический тип, если запрос будет содержать что-то
+# другое, он будет преобразован в логический тип данных
+
+# http://127.0.0.1:8000/itemsbool/foo?short=1
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id,"short":short}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
